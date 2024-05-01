512c0282eb5ff9d3984048024b8533a0134f517a
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2024-05-01 13:08:37 +0300

    mod task_6.3

--- js/script_6.3.js
+++ js/script_6.3.js
@@ -1,11 +1,11 @@
-function removeElement(array, item) {
-  const index = array.indexOf(item);
+function removeElement(num, item) {
+  const index = num.indexOf(item);
 
   if (index !== -1) {
-    array.splice(index, 1);
+    num.splice(index, 1);
   }
 }
 
-const array = [1, 3, 4, 6, 2, 5, 7];
-removeElement(array, 4);
-console.log(array);
+const num = [1, 3, 4, 6, 2, 5, 7];
+removeElement(num, 4);
+console.log(num);
